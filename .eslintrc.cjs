fb97791ca1a52006f9e570a5a01289822b6b4218
theproject93 <lcsferreira93@outlook.com>
2026-02-16 14:54:13 -0300

    feat: setup ESLint Prettier Tailwind extensions

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintcache'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    react: { version: '18.2' },
+  },
+};
